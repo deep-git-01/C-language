969f29881e26c028e2beb159000d8dd2f0c56ac3
Deep Ranpariya <ranpariyadeepchandubhai@gmail.com>
2023-10-26 18:12:05 +0200

    Meine erstes Programm MyName

---
+++ Praktikum/Code/MyName/myname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+ char name [4] = "Deep";
+   int date = 26;
+  int month = 2;
+  int year = 2001;
+
+  printf("Meine name ist %s\n",name);
+  printf("Meine Gebustdatum ist %02d.%02d.%02d\n",date,month,year);
+  
+  
+  return 0;
+}
+
